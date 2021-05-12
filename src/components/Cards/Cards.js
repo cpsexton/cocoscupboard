ae61250427c4b6e681e01c7832c4bca23235dd46
Chris Sexton <cpsexton@gmail.com>
2021-05-12 14:52:55 -0400

    herosection text makes more sense

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -5,14 +5,14 @@ import './Cards.css';
 function Cards() {
     return (
         <div className='cards'>
-            <h1>Check out these epic destinations!</h1>
+            <h1>Check out our cool shit!</h1>
             <div className='cards__container'>
                 <div className='cards__wrapper'>
                     <ul className='cards__items'>
                         <CardItem
                             src={require('../../images/img-9.jpg').default}
                             text='Check out this crazy shit'
-                            label='Adventure'
+                            label='Foster'
                             path='/services'
                         />
                         <CardItem
